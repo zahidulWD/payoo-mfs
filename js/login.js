74d6c4f35cd61208f298ff3a3bdcc668e8dca30f
zahidul <zahidul670791@gmail.com>
2024-09-18 10:48:25 -0700

    added home page

--- js/login.js
+++ js/login.js
@@ -4,10 +4,20 @@ document.getElementById("button-login")
 .addEventListener("click", function(event){
     // step-2 prevent default behavior (reloading browser)
     event.preventDefault();
-    console.log("click login button")
+    // console.log("click login button")
 
-    // get the phone number
-  
+    // step-3 get the phone number  and pin number
   const phoneNumber = document.getElementById("phone-number").value;
-  console.log(phoneNumber)  
+  const pinNumber = document.getElementById("pin-number").value;
+  console.log(phoneNumber,pinNumber)
+  
+   // step-4 validate phone ans pin
+    // good system na.....just
+
+   if (phoneNumber === '5' && pinNumber ==='1234'){
+    console.log("You are logged in");
+    window.location.href ='/home.html';
+   }else{
+    alert("wrong phone number or pin")
+   } 
 })
