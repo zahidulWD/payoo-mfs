dcd48144cb8ac833dcd68524232ea0e5deec7a21
zahidul <zahidul670791@gmail.com>
2024-09-18 11:28:16 -0700

    added partially the add money account

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
-console.log("added home")
+// Add money to the account
+// console.log("added home")
+
+
+// step-1: add an event handler to the add money button inside the from
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        
+        // step-2: get money to be added to the account
+        const addMoneyInput = document.getElementById("input-add-money").value;
+        console.log(addMoneyInput);
+
+        // step-3: get the pin number provided
+        const pinNumberInput = document.getElementById("input-pin-number").value;
+        console.log(pinNumberInput);
+    })
